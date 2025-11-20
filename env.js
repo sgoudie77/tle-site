38d864f19f00a3e843744d767df49333e7ff2c5a
sean goudie <sgoudie77@hotmail.com>
2025-11-20 14:52:35 +0700

    display feed update

--- env.js
+++ env.js
@@ -1,2 +1,6 @@
 const showFeed = document.querySelector('#discord-feed'); 
-showFeed.setAttribute('src', "https://discord.com/widget?id=1165276239716696184&theme=dark");
+const sec1 = "https://disco";
+const sec2 = "rd.com/widget?id=116527623";
+const sec3 = "9716696184&theme=dark";
+const full = sec1 + sec2 + sec3;
+showFeed.setAttribute('src', full);
